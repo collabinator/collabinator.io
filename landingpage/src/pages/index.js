ce9d8c3cfeeadf8885fe6c8d246e01beff0e1a8c
Jason Dudash <jason.dudash@gmail.com>
2025-06-22 09:47:38 -0400

    Update index.js

--- landingpage/src/pages/index.js
+++ landingpage/src/pages/index.js
@@ -34,18 +34,9 @@ const IndexPage = () => (
             </a>
           </div>
           <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-800">
-            <h2 class="text-lg sm:text-xl text-white font-medium title-font mb-2">Snowbooth</h2>
-            <p class="leading-relaxed text-base mb-4">TBA.</p>
-            <a class="text-yellow-400 inline-flex items-center">Coming Soon
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
-          </div>
-          <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-800">
-            <h2 class="text-lg sm:text-xl text-white font-medium title-font mb-2">VSCode Extension</h2>
-            <p class="leading-relaxed text-base mb-4">TBA.</p>
-            <a class="text-yellow-400 inline-flex items-center">In Planning Phase
+            <h2 class="text-lg sm:text-xl text-white font-medium title-font mb-2">mdbub</h2>
+            <p class="leading-relaxed text-base mb-4">A mindmap tool for your terminal..</p>
+            <a class="text-yellow-400 inline-flex items-center" href="https://github.com/collabinator/mdbubbles">Learn More
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
               </svg>
